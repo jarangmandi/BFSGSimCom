c8f5f166bc8d1c11b3e073d26f574803901f52ff
Andrew Parish <andrew@parishfamily.co.uk>
2015-05-12 01:04:31 +0100

    Made changes to the way TS3Channels works so that it maintains indexes by channelID and by frequency. Problem is that frequency may not be unique, but parent and frequency *should* be. Nested maps? Ugh. Minor change to ConsoleApplication1 to do stuff even if an FSUIPC connection isn't found. Removed unwanted file - FSUIPCLibrary/ReadMe.txt

--- BFSGSimCom/TS3Channels.h
+++ BFSGSimCom/TS3Channels.h
@@ -11,8 +11,19 @@ using namespace std;
 
 class TS3Channels
 {
+    struct channelInfo
+    {
+        uint64 channelID;
+        uint64 parentChannelID;
+        string channelName;
+        uint16_t frequency;
+
+        channelInfo(uint16_t channelFrequency, uint64 channelID, uint64 parentChannelID, string channelName);
+    };
+
 private:
-    map<uint16_t, uint64> channelMap;
+    map<uint16_t, channelInfo*> channelFrequencyMap;
+    map<uint64, channelInfo*> channelIDMap;
 
     uint16_t getFrequencyFromString(string);
 
@@ -22,7 +33,7 @@ public:
 
     static const uint64 CHANNEL_ID_NOT_FOUND = UINT64_MAX;
 
-    uint16_t addOrUpdateChannel(string, uint64);
+    uint16_t addOrUpdateChannel(string, uint64, uint64 parentChannel = 0);
     uint64 deleteChannel(uint64);
     void deleteAllChannels(void);
     uint64 getChannelID(uint16_t);
--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -25,27 +25,30 @@ int _tmain(int argc, _TCHAR* argv[])
         {
             FSUIPC_Process(&dwResult);
         }
-
-        tch.addOrUpdateChannel("122.80 - Unicom", 2);
-        tch.addOrUpdateChannel("118.30 - Tauranga Tower", 3);
-        tch.addOrUpdateChannel("123.450 - Andyville", 4);
-        tch.addOrUpdateChannel("123.425 - Missed A", 5);
-        tch.addOrUpdateChannel("Auckland Information - 121.10", 6);
-        tch.addOrUpdateChannel("Test - 234.45", 7);
-        tch.addOrUpdateChannel("Test2 - 123.46", 8);
-
-        result = tch.getChannelID(uint16_t(12345));
-        result = tch.getChannelID(uint16_t(11830));
-        result = tch.getChannelID(122.80);
-        result = tch.getChannelID(123.42);
-        result = tch.getChannelID(1.);
-        result = tch.getChannelID(uint16_t(12110));
-
-        if (result == TS3Channels::CHANNEL_ID_NOT_FOUND) printf("Ha");
     }
 
+    tch.addOrUpdateChannel("122.80 - Unicom", 2);
+    tch.addOrUpdateChannel("118.30 - Tauranga Tower", 3);
+    tch.addOrUpdateChannel("123.450 - Andyville", 4);
+    tch.addOrUpdateChannel("123.425 - Missed A", 5);
+    tch.addOrUpdateChannel("Auckland Information - 121.10", 6);
+    tch.addOrUpdateChannel("Test - 234.45", 7);
+    tch.addOrUpdateChannel("Test2 - 123.46", 8);
+    tch.deleteChannel(6);
+    tch.addOrUpdateChannel("Different Information - 121.10", 6);
+    tch.addOrUpdateChannel("123.45 - a", 5);
+
+    result = tch.getChannelID(uint16_t(12345));
+    result = tch.getChannelID(uint16_t(11830));
+    result = tch.getChannelID(122.80);
+    result = tch.getChannelID(123.42);
+    result = tch.getChannelID(uint16_t(12110));
+    result = tch.getChannelID(1.);
+
+    if (result == TS3Channels::CHANNEL_ID_NOT_FOUND) printf("Ha");
+
     FSUIPC_Close();
-    
-	return 0;
+
+    return 0;
 }
 
