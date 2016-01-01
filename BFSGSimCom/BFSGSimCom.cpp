40e53371a1dc9ed16bb46dd70882388ee2ff835e
Andrew Parish <andrew@parishfamily.co.uk>
2016-01-01 18:00:27 +0000

    Uprevved to version 0.7

--- BFSGSimCom/BFSGSimCom.cpp
+++ BFSGSimCom/BFSGSimCom.cpp
@@ -188,7 +188,7 @@ const char* ts3plugin_name() {
 
 /* Plugin version */
 const char* ts3plugin_version() {
-    return "0.6";
+    return "0.7";
 }
 
 /* Plugin API version. Must be the same as the clients API major version, else the plugin fails to load. */
