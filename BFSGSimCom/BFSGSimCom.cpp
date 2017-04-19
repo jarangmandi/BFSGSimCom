1226ca2126ebc9b6d59ab20e7debf34285606818
Andrew Parish <andrew.parish@cgi.com>
2017-04-19 15:00:55 +0100

    Uprevved version number in plugin to 0.9.2!

--- BFSGSimCom/BFSGSimCom.cpp
+++ BFSGSimCom/BFSGSimCom.cpp
@@ -366,7 +366,7 @@ const char* ts3plugin_name() {
 
 /* Plugin version */
 const char* ts3plugin_version() {
-    return "0.9.1";
+    return "0.9.2";
 }
 
 /* Plugin API version. Must be the same as the clients API major version, else the plugin fails to load. */
