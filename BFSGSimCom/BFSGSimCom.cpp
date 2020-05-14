29410326a370648b6438ca1c196d512c9d3096e7
Andrew Parish <andrew@parishfamily.co.uk>
2020-05-14 18:41:36 +0100

    Uprev to version 0.13.2

--- BFSGSimCom/BFSGSimCom.cpp
+++ BFSGSimCom/BFSGSimCom.cpp
@@ -382,7 +382,7 @@ const char* ts3plugin_name() {
 
 /* Plugin version */
 const char* ts3plugin_version() {
-    return "0.13.1";
+    return "0.13.2";
 }
 
 /* Plugin API version. Must be the same as the clients API major version, else the plugin fails to load. */
